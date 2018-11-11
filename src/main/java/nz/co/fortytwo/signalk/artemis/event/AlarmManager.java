ffc09c049ec83c92a33ce294547188784fe9fc40
rob42 <robert@42.co.nz>
2018-11-12 12:16:15 +1300

    Add alarm manager/test and DeltaMessageTransformer

---
+++ src/main/java/nz/co/fortytwo/signalk/artemis/event/AlarmManager.java
@@ -0,0 +1,40 @@
+package nz.co.fortytwo.signalk.artemis.event;
+
+
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mjson.Json;
+import nz.co.fortytwo.signalk.artemis.service.InfluxDbService;
+import nz.co.fortytwo.signalk.artemis.service.TDBService;
+import nz.co.fortytwo.signalk.artemis.util.Config;
+import nz.co.fortytwo.signalk.artemis.util.ConfigConstants;
+import nz.co.fortytwo.signalk.artemis.util.Util;
+
+
+public class AlarmManager {
+	private static Logger logger = LogManager.getLogger(AlarmManager.class);
+	protected static TDBService influx = new InfluxDbService();
+	private String vesselUuid=Config.getConfigProperty(ConfigConstants.UUID);
+	
+	public AlarmManager() {
+		logger.debug("Alarm manager created for vessel:{}",vesselUuid);
+	}
+	public void calculate(String uuid) {
+		logger.debug("Alarm check fired ");
+		influx.setWrite(true);
+		NavigableMap<String, Json> map = new ConcurrentSkipListMap<>();
+		Map<String, String> queryMap = new HashMap<>();
+		queryMap.put("skey",Util.regexPath(nav+dot+"position").toString());
+		queryMap.put("uuid",Util.regexPath(uuid).toString());
+		influx.loadData(map, vessels, queryMap);
+		logger.debug("Position: {}", map);
+	}
+}
---
+++ src/main/java/nz/co/fortytwo/signalk/artemis/transformer/DeltaMsgTransformer.java
@@ -0,0 +1,143 @@
+package nz.co.fortytwo.signalk.artemis.transformer;
+
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.GET;
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.dot;
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.nav_datetime;
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.self;
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.value;
+import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.vessels;
+
+import java.io.IOException;
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.api.core.ActiveMQExceptionType;
+import org.apache.activemq.artemis.api.core.ICoreMessage;
+import org.apache.activemq.artemis.api.core.Interceptor;
+import org.apache.activemq.artemis.api.core.Message;
+import org.apache.activemq.artemis.core.protocol.core.Packet;
+import org.apache.activemq.artemis.core.protocol.core.impl.wireformat.SessionSendMessage;
+import org.apache.activemq.artemis.core.server.transformer.Transformer;
+import org.apache.activemq.artemis.spi.core.protocol.RemotingConnection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mjson.Json;
+import nz.co.fortytwo.signalk.artemis.intercept.BaseInterceptor;
+import nz.co.fortytwo.signalk.artemis.service.SignalkMapConvertor;
+import nz.co.fortytwo.signalk.artemis.util.Config;
+import nz.co.fortytwo.signalk.artemis.util.ConfigConstants;
+import nz.co.fortytwo.signalk.artemis.util.Util;
+
+/*
+*
+* Copyright (C) 2012-2014 R T Huitema. All Rights Reserved.
+* Web: www.42.co.nz
+* Email: robert@42.co.nz
+* Author: R T Huitema
+*
+* This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+* WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+/**
+ * Converts SignalK delta format to map format
+ * 
+ * @author robert
+ * 
+ */
+
+public class DeltaMsgTransformer extends BaseInterceptor implements Transformer {
+
+	private static Logger logger = LogManager.getLogger(DeltaMsgTransformer.class);
+
+	/**
+	 * Reads Delta format JSON and creates a . Does nothing if json is
+	 * not an update, and returns the original message
+	 * 
+	 * @param node
+	 * @return
+	 */
+	@Override
+	public Message transform(Message message) {
+
+			
+			Json node = Util.readBodyBuffer(message.toCore());
+			
+			if (logger.isDebugEnabled())
+				logger.debug("Delta msg: {}", node.toString());
+
+			// deal with diff format
+			if (isDelta(node)) {
+				//try {
+					NavigableMap<String, Json> map = processDelta(node);
+					if (!influx.getWrite()) {
+						// set the time if we can
+						// vessels.urn:mrn:signalk:uuid:80a3bcf0-d1a5-467e-9cd9-35c1760bb2d3.navigation.datetime.values.NMEA0183.SERIAL.value
+						String uuid = Config.getConfigProperty(ConfigConstants.UUID);
+						
+						for (String key : map.keySet()) {
+							if (logger.isDebugEnabled())
+								logger.debug("Check key: {} starts with {}", key, vessels+dot+uuid+dot + nav_datetime);
+							if (key.startsWith(vessels+dot+uuid+dot + nav_datetime)||key.startsWith(vessels+dot+self+dot + nav_datetime)) {
+								Json time = map.get(key);
+								if (time != null && !time.isNull()) {
+									// set system time
+									// sudo date -s 2018-08-11T17:52:51+12:00
+									String cmd = "sudo date -s " + time.at(value).asString();
+									logger.info("Executing date setting command: {}", cmd);
+									try {
+										Runtime.getRuntime().exec(cmd.split(" "));
+										logger.info("Executed date setting command: {}", cmd);
+										influx.setWrite(true);
+									} catch (IOException e) {
+										logger.error(e,e);
+									}
+								}
+							}
+						}
+					}
+					//saveMap(map);
+					
+					map.forEach((k,j) -> {
+						try {
+							sendInternal(k, j);
+						} catch (Exception e) {
+							logger.error(e,e);
+						}
+					});
+					return message;
+//				} catch (Exception e) {
+//					logger.error(e, e);
+//					throw new ActiveMQException(ActiveMQExceptionType.INTERNAL_ERROR, e.getMessage(), e);
+//				}
+			}
+			return message;
+	}
+
+	protected NavigableMap<String, Json> processDelta(Json node) {
+		if (logger.isDebugEnabled())
+			logger.debug("Saving delta: {}", node.toString());
+		NavigableMap<String, Json> map = new ConcurrentSkipListMap<>();
+		SignalkMapConvertor.parseDelta(node, map);
+		return map;
+
+	}
+
+	
+
+}
